177cf20f6b85ff823b7b8aa11744a3497381383f
邓超 <dcdengchao@qq.com>
2023-08-08 17:27:22 +0800

    fix issue

--- checkin.sh
+++ checkin.sh
@@ -11,17 +11,18 @@ get_json_value() {
 
 echo '------------------sign------------------'
 sign_result=$(curl -H "cookie:${COOKIE}" -H 'content-type:application/json;charset=UTF-8' -d '{"token": "glados.one"}' -X POST 'https://glados.rocks/api/user/checkin')
-# sign_message=$(echo "$sign_result" | grep -Eo '"message":"[^"]*"')
-sign_message=$(get_json_value "$sign_result" message) 
+echo "$sign_result" | grep -Eo '"message":"[^"]*"'
+#sign_message=$(echo "$sign_result" | grep -Eo '"message":"[^"]*"')
+#sign_message=$(get_json_value "$sign_result" message) 
 
-echo '-----------------status-----------------'
-status_result=$(curl -H "cookie:${COOKIE}" -X GET 'https://glados.rocks/api/user/status')
+#echo '-----------------status-----------------'
+#status_result=$(curl -H "cookie:${COOKIE}" -X GET 'https://glados.rocks/api/user/status')
 # status_days=$(echo "$status_result" | grep -Eo '"leftDays":"[^"]*"')
-status_days=$(get_json_value "$status_result" leftDays)
-status_days=$(echo "$status_days" | bc -l | cut -d '.' -f 1)
+#status_days=$(get_json_value "$status_result" leftDays)
+#status_days=$(echo "$status_days" | bc -l | cut -d '.' -f 1)
 
-text="GLaDOS: $sign_message 剩余 $status_days 天"
-echo "GLaDOS: $sign_message 剩余 $status_days 天"
+#text="GLaDOS: $sign_message 剩余 $status_days 天"
+#echo "GLaDOS: $sign_message 剩余 $status_days 天"
 # text=${text//\"/}
 # text="${text// /%20}"
 # 将字符串进行URL编码
